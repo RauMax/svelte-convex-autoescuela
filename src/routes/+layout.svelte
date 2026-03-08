25f478c158fda2333d8c9bd591dea2a5241ddcca
RauMax <maximilianoerau@gmail.com>
2026-03-08 01:10:56 +0000

    Fix v3: Remove Convex from layout for initial static landing page deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
-  import { env } from "$env/dynamic/public";
-  import { setupConvex } from "convex-svelte";
-
   let { children } = $props();
-  setupConvex(env.PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
