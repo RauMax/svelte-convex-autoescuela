4b23e63ef43fb812a038989adcafbca480678bdf
RauMax <maximilianoerau@gmail.com>
2026-03-08 01:09:36 +0000

    Fix v2: Use dynamic env variables to avoid Vercel build error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
-	import { setupConvex } from 'convex-svelte';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { env } from "$env/dynamic/public";
+  import { setupConvex } from "convex-svelte";
 
-	let { children } = $props();
-	setupConvex(PUBLIC_CONVEX_URL);
+  let { children } = $props();
+  setupConvex(env.PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
